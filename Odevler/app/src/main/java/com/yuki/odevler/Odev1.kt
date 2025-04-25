1adb0fddaa5efde3c4caf3ec5b4fc93af479ccc7
meyra <meyramaden@gmail.com>
2025-04-25 03:20:15 +0300

    Ödev 1: Değişken tanımlamaları ve çıktı işlemleri

---
+++ Odevler/app/src/main/java/com/yuki/odevler/Odev1.kt
@@ -0,0 +1,79 @@
+package com.yuki.odevler
+
+fun main() {
+    val sehir = "Kocaeli"
+    val ulke = "Turkiye"
+    val telefon = "05454647534"
+    val postaKodu = "41400"
+    var email = "meyramaden@gmail.com"
+    val meslek = " yazilim mühendisi"
+    val stokMiktari = "300"
+    val musteriAdi = "meyra"
+    val bakiye = 2323
+    val dogumGunu = "30.04.2004"
+    val medeniDurum = true  //true kadın, false erkek
+    val maas = 30000
+    val urunYorum = "guzel"
+
+    println(
+        "sehir: $sehir ulke: $ulke posta kodu: $postaKodu" +
+                "\nmusteri adi: $musteriAdi dogum gunu: $dogumGunu medeni durum: $medeniDurum\"+telefon: $telefon email: $email " +
+                "meslek: $meslek maas: $maas " +
+                "\nurun yorum: $urunYorum stok miktari: $stokMiktari  +bakiye: $bakiye"
+    )
+
+    val odemeTarihi = "21.10.2025"
+    val odeme = 23.99
+    val siparisAdeti = 1000
+    val arabaModeli = "togg"
+    val kitapAdi = "fahrenheit 451"
+    val yayinTarihi = "21.02.1989"
+    var indirimMiktari = 100
+    val odaSayisi = 3
+    val enlem = 30
+    val boylam = 40
+    val urunAdi = "araba"
+    var yemekFiyati = 120
+
+    println(
+        "odeme tarihi: $odemeTarihi odeme: $, odeme siparis adeti: $siparisAdeti, araba modeli: $arabaModeli" +
+                "\nkitap adi: $kitapAdi yayin tarihi: $yayinTarihi indirim miktari: $indirimMiktari oda sayisi: $odaSayisi" +
+                "\nenlem: $enlem boylam: $boylam urun adi: $urunAdi yemek fiyati: $yemekFiyati"
+    )
+
+    val marka = "a markasi"
+    val muzikAdi = "dear john"
+    val videoSuresi = 643   //saniye
+    val urunPuani = 9.2
+    val resimAdi = "manzara"
+    val dosyaFormati = ".png"
+    var renk = "mavi"
+    val renkKodu = "blue"
+    val telefonModeli = "redmi note 10S"
+    val ekranBoyutu = 24
+    val agirlik = 300
+    println(
+        "marka: $marka muzik adi: $muzikAdi video suresi: $videoSuresi urun puani: $urunPuani" +
+                "\nresim: $resimAdi dosya format: $dosyaFormati renk: $renk renk kodu: $renkKodu" +
+                "\ntelefon modeli: $telefonModeli ekran boyutu: $ekranBoyutu agirlik: $agirlik"
+    )
+
+    val ulusalGun = "23 Nisan"
+    val tatilGunu = "23 Nisan"
+    val rezervasyonTarihi = "30 Nisan"
+    val sokakAdi = "yenikent"
+    val otobusHatti = "476B"
+    var kalanDakika = 25
+    val takipKodu = 41400
+    var kuponSuresi = 480
+    val kuponKodu = 1756
+    val faturaAdresi = "Gebze"
+    println(
+        "ulusal gun: $ulusalGun, tatil gunu: $tatilGunu" +
+                "\nsokak adı: $sokakAdi otobus hattı: $otobusHatti kalan dakika: $kalanDakika fatura adresi: $faturaAdresi"
+    )
+}
+
+
+
+
